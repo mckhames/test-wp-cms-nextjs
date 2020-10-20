2dbdf1be77f0fdc0a7e0919ea0a8453085a15b40
McKenzie <mckhames@gmail.com>
2020-10-20 14:14:53 -0400

    redeployment testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/index.css'
-import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-import 'https://fonts.googleapis.com/icon?family=Material+Icons'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
