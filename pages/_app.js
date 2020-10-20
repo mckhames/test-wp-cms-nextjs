6c63a7c47ce1afe6f491327faf40a0e7cdab999e
McKenzie <mckhames@gmail.com>
2020-10-20 11:12:19 -0400

    2nd commit, added material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/index.css'
+import 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+import 'https://fonts.googleapis.com/icon?family=Material+Icons'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
