4147dee7e63bf07519ebdf640104a4ab1a2a96f8
McKenzie Hames <mckhames@gmail.com>
2020-10-22 09:29:33 -0400

    Added bootstrap nav to test page

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navigation from '../components/navigation'
 
 export default function Test (){
-    return <h1>This is a test</h1>
+    return 
+    <Navigation />
+    <h1>This is a test</h1>
 };
